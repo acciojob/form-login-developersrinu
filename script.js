cdbd50ebde443426fe0e17523b2f8f5310fa15d7
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-07-02 16:41:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
 
+
+function getFormvalue() {
+    const firstName = Document.getElementByTagName(input)[0].value
+	 const lastName = Document.getElementByTagName(input)[1].value
+	return firstName+lastName;
 }
